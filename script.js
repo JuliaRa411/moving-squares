0cdb48fc7293e01f6827b6fb3404f1c4a4f52e2c
Julia Rakityanska <96792225+JuliaRa411@users.noreply.github.com>
2022-03-08 17:09:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+  
+gsap.to(".move", {x:350, duration: 3 , rotation: 360,  stagger:0.5, duration: 1.5, scale:2.4, 
+   
+     
+}); 
+    
+gsap.to(".moveOne", {x:550, duration: 3 , rotation: 360,  stagger:0.5, duration: 1.5, scale:2.4,
+   
+     
+}); 
+    
+    
+gsap.to(".moveTwo", {x:750, duration: 3 , rotation: 360,  stagger:0.5, duration: 1.5, scale: 2.4,
+   
+     
+}); 
+
+gsap.to(".one", {   rotation: 360,  duration: 10, repeat: -1, });
+gsap.to(".two",  {  rotation: 360,  duration: 10, repeat: -1}); 
+gsap.to(".three",  { rotation: 360,   duration: 10, repeat: -1}); 
+gsap.to(".four", { rotation: 360,  duration: 10, repeat: -1}); 
+gsap.to(".five", { rotation: 360,  duration: 10, repeat: -1}); 
+ 
+ 
